1071a146f9861897cb57a368e12b2a267bfee8f1
jerick-cumayas <cumayas.jerick@gmail.com>
2025-09-03 09:42:24 +0800

    [SILVER]
    - checking for null or duplicate primary keys
    - unwanted spaces in string fields
    - data standardization and consistency
    - invalid date ranges and orders
    - data consistency between related fields
    - tables are truncated before any other operation
    - records from tables of the bronze layer and cleansed and are performed
      with data normalization before inserting to the silver layer

--- scripts/silver/data-checking.sql
+++ scripts/silver/data-checking.sql
@@ -1,3 +1,17 @@
+/*
+=================================================
+DATA CLEANING: LOAD SILVER LAYER (BRONZE -> SILVER)
+=================================================
+Script purpose:
+	This script performs various quality checks for data consistency, accuracy,
+	and standardization across the silver schema. It includes checks for:
+		- Null or duplicate primary keys
+		- Unwanted spaces in string fields
+		- Data standardization and consistency
+		- Invalid date ranges and orders.
+		- Data consistency between related fields.
+*/
+
 -- Check for nulls or duplicates in primary key of bronze.crm_cust_info
 -- Expectation: No result
 SELECT 
@@ -29,3 +43,83 @@ SELECT prd_cost FROM bronze.crm_prd_info WHERE prd_cost < 0 or prd_cost IS NULL;
 -- Check start and end date columns
 -- Expectation: No results
 SELECT * FROM silver.crm_prd_info WHERE prd_end_dt < prd_start_dt;
+
+-- Check for Invalid Dates
+SELECT 
+NULLIF(sls_due_dt,0) AS sls_due_dt
+FROM silver.crm_sales_details
+WHERE 
+sls_due_dt <= 0 OR 
+len(sls_due_dt) != 8 OR 
+sls_due_dt > 20500101 OR 
+sls_due_dt < 19000101
+
+-- Check for Invalid Date orders
+SELECT 
+*
+FROM silver.crm_sales_details
+WHERE sls_order_dt > sls_ship_dt OR sls_order_dt > sls_due_dt
+
+-- Check Sales, Quantity, Price
+SELECT DISTINCT
+sls_sales,
+sls_quantity,
+sls_price
+FROM silver.crm_sales_details 
+WHERE 
+sls_sales IS NULL OR sls_quantity IS NULL or sls_price IS NULL OR
+sls_sales <= 0 OR sls_quantity <= 0 or sls_price <= 0 OR
+sls_sales != sls_quantity * sls_price
+ORDER BY sls_sales, sls_quantity, sls_price
+
+SELECT * FROM silver.crm_sales_details
+
+-- Identify out-of-range dates
+-- Bad data
+SELECT DISTINCT
+bdate 
+FROM silver.erp_cust_az12
+WHERE bdate < '1924-01-01' OR bdate > GETDATE()
+
+-- Check gender values
+SELECT DISTINCT gen FROM silver.erp_cust_az12;
+
+-- Check cst_ids that are not in customer info
+SELECT * FROM bronze.erp_loc_a101;
+SELECT 
+cst_key 
+FROM silver.crm_cust_info
+WHERE cst_key NOT IN (
+	SELECT
+	REPLACE(cid, '-', '') AS cid
+	FROM bronze.erp_loc_a101
+)
+
+-- Data Standardization and Consistency
+SELECT DISTINCT REPLACE(cid, '-', '') AS cid FROM bronze.erp_loc_a101;
+SELECT DISTINCT cntry FROM silver.erp_loc_a101 ORDER BY cntry;
+SELECT DISTINCT cntry FROM (
+	SELECT
+	REPLACE(cid, '-', '') AS cid,
+	CASE 
+		WHEN TRIM(cntry) IN ('USA', 'US', 'United States') THEN 'United States'
+		WHEN TRIM(cntry) = 'DE' THEN 'Germany'
+		WHEN TRIM(cntry) = '' OR cntry IS NULL THEN 'n/a'
+		ELSE TRIM(cntry)
+	END AS cntry
+	FROM bronze.erp_loc_a101
+) AS sub;
+
+-- Check for unwanted spaces
+SELECT * 
+FROM bronze.erp_px_cat_g1v2 
+WHERE cat != TRIM(cat) OR subcat != TRIM(subcat) OR maintenance != TRIM(maintenance);
+
+SELECT DISTINCT cat 
+FROM bronze.erp_px_cat_g1v2
+
+SELECT DISTINCT subcat 
+FROM bronze.erp_px_cat_g1v2
+
+SELECT DISTINCT maintenance 
+FROM bronze.erp_px_cat_g1v2
--- scripts/silver/data-cleaning.sql
+++ scripts/silver/data-cleaning.sql
@@ -47,7 +47,7 @@ END cst_gndr,
 cst_create_date
 FROM bronze.crm_cust_info;
 
--- FULL CODE DATA CLEANSING
+-- DATA CLEANING: CUSTOMER INFO
 SELECT 
 cst_id,
 cst_key,
@@ -72,7 +72,10 @@ FROM (
 )t WHERE rank_by_date = 1;
 
 -- INSERT INTO silver layer table
+PRINT 'PERFORMING INSERTION TO CRM CUSTOMER INFO'
+PRINT 'TRUNCATING SILVER.CRM_CUST_INFO'
 TRUNCATE TABLE silver.crm_cust_info;
+PRINT '>> INSERTING DATA INTO: silver.crm_cust_info'
 INSERT INTO silver.crm_cust_info (
 	cst_id,
 	cst_key,
@@ -107,41 +110,11 @@ FROM (
 
 
 --------------
-
-SELECT * FROM bronze.crm_prd_info;
-
-SELECT DISTINCT(prd_line) FROM bronze.crm_prd_info;
-
-SELECT sls_prd_key FROM bronze.crm_sales_details;
-
-SELECT 
-prd_id, 
-prd_key, 
-prd_nm, 
-prd_start_dt,
-prd_end_dt,
-LEAD (prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt)-1 AS prd_end_dt_test
-FROM bronze.crm_prd_info
-WHERE prd_key in ('AC-HE-HL-U509-R');
-
-SELECT 
-prd_id,
-prd_key,
-REPLACE(SUBSTRING(prd_key, 1, 5), '-','_') AS cat_id,
-SUBSTRING(prd_key, 7, LEN(prd_key) - 6) AS prd_key,
-prd_nm, 
-ISNULL(prd_cost, 0) AS prd_cost,
-CASE UPPER(TRIM(prd_line))
-	WHEN 'M' THEN 'Mountain'
-	WHEN 'R' THEN 'Road'
-	WHEN 'S' THEN 'Other Sales'
-	WHEN 'T' THEN 'Touring'
-	ELSE 'n/a'
-END AS prd_line,
-CAST(prd_start_dt AS DATE) AS prd_start_dt,
-CAST(LEAD (prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt)-1 AS DATE) AS prd_end_dt
-FROM bronze.crm_prd_info;
-
+-- DATA CLEANING: PRODUCT INFO
+PRINT 'PERFORMING INSERTION TO CRM PRODUCT INFO'
+PRINT 'TRUNCATING SILVER.CRM_PRD_INFO'
+TRUNCATE TABLE silver.crm_prd_info;
+PRINT '>> INSERTING DATA INTO: silver.crm_prd_info'
 INSERT INTO silver.crm_prd_info (
 	prd_id,
 	cat_id,
@@ -168,3 +141,110 @@ END AS prd_line,
 CAST(prd_start_dt AS DATE) AS prd_start_dt,
 CAST(LEAD (prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt)-1 AS DATE) AS prd_end_dt
 FROM bronze.crm_prd_info;
+
+-- DATA CLEANING: CRM SALES AND ORDERS
+PRINT 'PERFORMING INSERTION TO CRM SALES DETAILS'
+PRINT 'TRUNCATING SILVER.CRM_SALES_DETAILS'
+TRUNCATE TABLE silver.crm_sales_details;
+PRINT '>> INSERTING DATA INTO: silver.crm_sales_details'
+INSERT INTO silver.crm_sales_details (
+	sls_ord_num,
+	sls_prd_key,
+	sls_cust_id,
+	sls_order_dt,
+	sls_ship_dt,
+	sls_due_dt,
+	sls_sales,
+	sls_quantity,
+	sls_price
+)
+SELECT 
+	sls_ord_num,
+	sls_prd_key,
+	sls_cust_id,
+	CASE 
+		WHEN sls_order_dt = 0 OR LEN(sls_order_dt) != 8 THEN NULL
+		ELSE CAST(CAST(sls_order_dt AS VARCHAR) AS DATE)
+	END AS sls_order_dt,
+	CASE 
+		WHEN sls_ship_dt = 0 OR LEN(sls_ship_dt) != 8 THEN NULL
+		ELSE CAST(CAST(sls_ship_dt AS VARCHAR) AS DATE)
+	END AS sls_ship_dt,
+	CASE 
+		WHEN sls_due_dt = 0 OR LEN(sls_due_dt) != 8 THEN NULL
+		ELSE CAST(CAST(sls_due_dt AS VARCHAR) AS DATE)
+	END AS sls_due_dt,
+	CASE 
+		WHEN sls_sales IS NULL OR sls_sales <= 0 OR sls_sales != sls_quantity * ABS(sls_price) 
+			THEN sls_quantity * ABS(sls_price)
+		ELSE sls_sales
+	END AS sls_sales, -- Recalculate sales if original value is missing or incorrect
+	sls_quantity,
+	CASE 
+		WHEN sls_price IS NULL OR sls_price <= 0 
+			THEN sls_sales / NULLIF(sls_quantity, 0)
+		ELSE sls_price  -- Derive price if original value is invalid
+	END AS sls_price
+FROM bronze.crm_sales_details;
+
+-- DATA CLEANING: ERP CUSTOMER AZ 12
+PRINT 'PERFORMING INSERTION TO ERP CUSTOMER'
+PRINT 'TRUNCATING SILVER.ERP_CUST_AZ12'
+TRUNCATE TABLE silver.erp_cust_az12;
+PRINT '>> INSERTING DATA INTO: silver.erp_cust_az12'
+INSERT INTO silver.erp_cust_az12 (
+	cid, 
+	bdate,
+	gen
+)
+SELECT
+CASE WHEN cid LIKE 'NAS%' THEN SUBSTRING (cid, 4, LEN(cid) - 3) -- Remove 'NAS' prefix if present
+	ELSE cid
+END AS cid,
+CASE  
+	WHEN bdate > GETDATE() THEN NULL -- Remove birth dates that are beyond the present date
+	ELSE bdate
+END AS bdate,
+CASE 
+	WHEN UPPER(TRIM(gen)) IN ('F', 'FEMALE') THEN 'FEMALE' -- Normalize gender avlues and handle unknown cases
+	WHEN UPPER(TRIM(gen)) IN ('M', 'MALE') THEN 'MALE'
+	ELSE 'n/a'
+END AS gen
+FROM bronze.erp_cust_az12;
+
+-- DATE CLEANING: ERP LOC
+PRINT 'PERFORMING INSERTION TO ERP LOCATION'
+PRINT 'TRUNCATING SILVER.ERP_LOC_A101'
+TRUNCATE TABLE silver.erp_loc_a101;
+PRINT '>> INSERTING DATA INTO: silver.erp_loc_a101'
+INSERT INTO silver.erp_loc_a101 (
+	cid,
+	cntry
+)
+SELECT
+REPLACE(cid, '-', '') AS cid, -- Replace the minus sign with empty string 
+CASE -- Date normalization with country column
+	WHEN TRIM(cntry) IN ('USA', 'US', 'United States') THEN 'United States'
+	WHEN TRIM(cntry) = 'DE' THEN 'Germany'
+	WHEN TRIM(cntry) = '' OR cntry IS NULL THEN 'n/a'
+	ELSE TRIM(cntry)
+END AS cntry
+FROM bronze.erp_loc_a101;
+
+-- DATA CLEANING: ERP PX CAT
+PRINT 'PERFORMING INSERTION TO ERP PX CAT'
+PRINT 'TRUNCATING SILVER.ERP_PX_CAT_G1V2'
+TRUNCATE TABLE silver.erp_px_cat_g1v2;
+PRINT '>> INSERTING DATA INTO: silver.erp_px_cat_g1v2'
+INSERT INTO silver.erp_px_cat_g1v2 (
+	id,
+	cat,
+	subcat,
+	maintenance
+)
+SELECT 
+id, 
+cat,
+subcat,
+maintenance 
+FROM bronze.erp_px_cat_g1v2
